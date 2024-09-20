d481d25710bed9ef8e01da6e14ab07eb0001b0d4
Kauah <menezeskauah@gmail.com>
2024-09-20 20:42:13 -0300

    cria app modulo users, configurar banco de dados postgres

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,9 +1,31 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UsersModule } from './users/users.module';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import * as path from 'path';
+
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: ['.env.development.local'],
+    }), 
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.LOCAL_HOST,
+      port: Number(process.env.LOCAL_PORT),
+      username: process.env.LOCAL_USERNAME,
+      password: process.env.LOCAL_PASSWORD,
+      database: process.env.LOCAL_DATABASE,
+      entities: [path.resolve(__dirname, '**', '*.entity{.ts,.js}')],
+      migrations: [],
+      migrationsRun: false,
+      synchronize: true,
+    }),
+    UsersModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
