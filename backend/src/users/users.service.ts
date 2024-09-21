59888b6095e9c4c2353d9abcab23832b72e92e42
Kauah <menezeskauah@gmail.com>
2024-09-20 22:18:10 -0300

    cria service do  Users

--- backend/src/users/users.service.ts
+++ backend/src/users/users.service.ts
@@ -1,4 +1,25 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Users } from './entities/user.entity';
+import { Repository } from 'typeorm';
+
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+    constructor(
+        @InjectRepository(Users)
+        private usersRepository: Repository<Users>,
+    ) {};
+
+    findAll():Promise<Users[]> {
+        return this.usersRepository.find();
+    };
+
+    findOne(id:number):Promise<Users | null> {
+        return this.usersRepository.findOneBy({ id });
+    };
+
+    createUser():Promise<Users> {
+        return;
+    };
+}
