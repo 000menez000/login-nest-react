fb9a8ecb5449fc0c48c82a05141dd19622734ebf
Kauah <menezeskauah@gmail.com>
2024-09-22 21:47:43 -0300

    cria rotas para roles

--- backend/src/roles/roles.controller.ts
+++ backend/src/roles/roles.controller.ts
@@ -1,4 +1,23 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Param, Post, Body } from '@nestjs/common';
+import { RolesEntity } from './entity/roles.entity';
+import { RolesService } from './roles.service';
 
 @Controller('roles')
-export class RolesController {}
+export class RolesController {
+    constructor(private readonly rolesService:RolesService) {};
+
+    @Get()
+    async findAll():Promise<RolesEntity[]> {
+        return await this.rolesService.findAll();
+    };
+
+    @Get(':id')
+    async findOne(@Param('id') roleId:number):Promise<RolesEntity> {
+        return await this.rolesService.findOne(roleId);
+    }
+
+    @Post('create-role')
+    async createRole(@Body() createRole:RolesEntity):Promise<RolesEntity> {
+        return await this.rolesService.createRole(createRole);    
+    };  
+}
--- backend/src/roles/roles.service.ts
+++ backend/src/roles/roles.service.ts
@@ -1,4 +1,36 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { Repository } from 'typeorm';
+import { RolesEntity } from './entity/roles.entity';
+import { InjectRepository } from '@nestjs/typeorm';
 
 @Injectable()
-export class RolesService {}
+export class RolesService {
+    constructor(
+        @InjectRepository(RolesEntity) 
+        private readonly rolesRepository:Repository<RolesEntity>
+    ) {};
+
+    async findAll():Promise<RolesEntity[]> {
+        return await this.rolesRepository.find();
+    };
+
+    async findOne(roleId):Promise<RolesEntity> {
+        const role = await this.rolesRepository.findOne({
+            where: {
+                id: roleId,
+            },
+        });
+
+        if (!role) {
+            throw new NotFoundException(`roleId <${roleId}> not found!`);
+        };
+
+        return role;
+    };
+
+    async createRole(rolesEntity):Promise<RolesEntity> {
+        return await this.rolesRepository.save({
+            ...rolesEntity,
+        });
+    }
+}
